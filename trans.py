bca6465e37add9391c09734831d5a672b25480b9
Vitor.chagas <vitor.chagas@pixforce.ai>
2025-02-23 15:55:31 -0300

    corrjindo identação

--- trans.py
+++ trans.py
@@ -5,7 +5,8 @@ from datetime import datetime
 import whisper
 import sys
 
-sys.path.append('/path/to/ffmpeg')
+sys.path.append("/path/to/ffmpeg")
+
 
 class AudioTranscriptionBot:
     """
@@ -13,7 +14,7 @@ class AudioTranscriptionBot:
     and sending the transcribed text back to the API.
     """
 
-    def __init__(self, config_file: str = 'conf.json'):
+    def __init__(self, config_file: str = "conf.json"):
         """
         Initializes the bot by loading configuration settings.
 
@@ -29,7 +30,7 @@ class AudioTranscriptionBot:
         :param file_name: The name of the JSON file containing the configuration.
         :return: A dictionary with the configuration.
         """
-        with open(file_name, 'r') as file:
+        with open(file_name, "r") as file:
             config = json.load(file)
         return config
 
@@ -41,28 +42,36 @@ class AudioTranscriptionBot:
         """
         headers = {
             "Content-Type": "application/json",
-            "X-API-KEY": self.config['API'].get('token')
+            "X-API-KEY": self.config["API"].get("token"),
         }
         session = requests.Session()
-        response = session.get('https://api.infoverse.com.br/v1/Bot_adm/file_transcription/1', headers=headers)
+        response = session.get(
+            "https://api.infoverse.com.br/v1/Bot_adm/file_transcription/1",
+            headers=headers,
+        )
+        print(response.json())
+        # escrenvendo json em arquivvo.txt
+        with open("response.txt", "w") as file:
+            file.write(json.dumps(response.json()))
 
-        if response.json()['status']:
-            data = response.json()['data'][0]
-            base64_string = data['base64']
-            id_message = data['id_message']
-            
-            with open('audio.txt', 'w') as file:
+        if response.json()["status"]:
+            print("dir:", response.json())
+            data = response.json()["data"][0]
+            base64_string = data["base64"]
+            id_message = data["id_message"]
+
+            with open("audio.txt", "w") as file:
                 file.write(base64_string)
 
             sound_data = base64.b64decode(base64_string)
-            with open('audio.mp3', 'wb') as file:
+            with open("audio.mp3", "wb") as file:
                 file.write(sound_data)
 
             return id_message
         else:
             return None
 
-    def mp3_to_text(self, mp3_file_path: str = 'audio.mp3') -> str:
+    def mp3_to_text(self, mp3_file_path: str = "audio.mp3") -> str:
         """
         Converts an MP3 file to text using the Whisper model.
 
@@ -72,13 +81,13 @@ class AudioTranscriptionBot:
         try:
             start_time = datetime.now()
             result = self.model.transcribe(mp3_file_path)
-            text = result['text']
+            text = result["text"]
             processing_time = datetime.now() - start_time
             print(f"Processing time: {processing_time}")
             return text
         except Exception as e:
             print(f"Error during transcription: {e}")
-            return 'Transcription error'
+            return "Transcription error"
 
     def send_text(self, text: str, id_message: str):
         """
@@ -89,15 +98,16 @@ class AudioTranscriptionBot:
         """
         headers = {
             "Content-Type": "application/json",
-            "X-API-KEY": self.config['API'].get('token')
+            "X-API-KEY": self.config["API"].get("token"),
         }
         session = requests.Session()
-        payload = {
-            'transcrition_text': text,
-            'id': id_message
-        }
+        payload = {"transcrition_text": text, "id": id_message}
 
-        response = session.post('https://api.infoverse.com.br/v1/Bot_adm/file_transcription', headers=headers, data=json.dumps(payload))
+        response = session.post(
+            "https://api.infoverse.com.br/v1/Bot_adm/file_transcription",
+            headers=headers,
+            data=json.dumps(payload),
+        )
         print(f"Response status code: {response.status_code}")
         print(f"Response text: {response.text}")
         print(f"Transcribed text: {text}")
@@ -109,11 +119,12 @@ class AudioTranscriptionBot:
         while True:
             id_message = self.get_messages()
             if id_message is not None:
-                transcribed_text = self.mp3_to_text('audio.mp3')
+                transcribed_text = self.mp3_to_text("audio.mp3")
                 self.send_text(transcribed_text, id_message)
             else:
-                print('\rNo messages found', end='', flush=True)
+                print("\rNo messages found", end="", flush=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bot = AudioTranscriptionBot()
     bot.run()
